999fe3e356f11e13622c16e1a3c15b3e7e46d463
BigBigMiao <BigBigMiao@github.com>
2020-09-28 17:08:52 -0700

    init

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tensorflow;
+using static Tensorflow.Binding;
+
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tf.compat.v1.disable_eager_execution();
+            var session = Session.LoadFromSavedModel("./");
+
+            foreach(var op in session.graph)
+            {
+                Console.WriteLine(op.name);
+                if(op.NumInputs > 0)
+                {
+                    Console.WriteLine(op.inputs[0].name);
+                }
+            }
+        }
+    }
+}
